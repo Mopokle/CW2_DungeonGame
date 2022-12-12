60614eae95cef046bb18b135a05278f86ddfea61
Kris <kris_k16@hotmail.com>
2022-12-12 10:14:02 +0000

    map

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -1,12 +1,19 @@
+
 extends CanvasLayer
 
 func _ready():
 	$PanelContainer/MarginContainer/Rows/CenterContainer/VBoxContainer/StartButton.grab_focus()
-	
-func _on_StartButton_pressed():
-	get_tree().paused = false
-	get_tree().change_scene("res://World.tscn")
 
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func _on_Start_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://World.tscn")
+	
+
+
+func _on_start_tab_clicked(tab):
+	get_tree().paused = false
+	get_tree().change_scene("res://World.tscn")
